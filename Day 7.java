90cbcd289323df756eb331adbf5bcd637b33562b
acoiqbal <acoikbalikbal@gmail.com>
2024-09-08 14:44:18 +0800

    Hari 7

---
+++ Day 7.java
@@ -0,0 +1,23 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Aco iqbal";
+        String nim = "D0223041";
+        int usia = 20;
+        char kelas = 'E';
+        double tinggi = 171.2;
+        
+       System.out.println(nama);
+        
+        System.out.println(nim);
+        
+       System.out.println(usia);
+        
+        System.out.println(kelas);
+        
+        System.out.println(tinggi);
+        
+        
+     }
+
+}
