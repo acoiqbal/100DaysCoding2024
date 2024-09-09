08ebeeda13fc8828e73cf4dd818b9bbb0ecb2f08
acoiqbal <acoikbalikbal@gmail.com>
2024-09-09 17:02:43 +0800

    Hari 8

---
+++ Day 8.java
@@ -0,0 +1,33 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String sc = "Aco iqbal";
+        String kl = "D0223041";
+        int us = 20;
+        char kj = 'E';
+        double it = 171.2;
+        
+        System.out.println(sc);
+        System.out.println(kl);
+        System.out.println(us);
+        System.out.println(kj);
+        System.out.println(it);
+        
+        System.out.println("--------------");
+        //update variabel
+        
+        sc = "apel";
+        kl = "merah";
+        us = 10;
+        kj = 'A';
+        it = 0.4;
+        
+        System.out.println("Nama buah :" +sc);
+        System.out.println("Warna :" +kl);
+        System.out.println("Jumlah apel :" +us);
+        System.out.println("kualitas :" +kj);
+        System.out.println("berat :" +it);
+        
+     }
+
+}
