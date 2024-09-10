655bd200de64536bbe626de44294454231ef38ec
acoiqbal <acoikbalikbal@gmail.com>
2024-09-10 17:20:31 +0800

    Hari 9

---
+++ Day 9.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        final int a = 20;
+        final double b = 14.5;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
