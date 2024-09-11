4afeda76d196ad149874fc0d15b2b0120496b1a3
acoiqbal <acoikbalikbal@gmail.com>
2024-09-11 18:07:53 +0800

    Hari 10

---
+++ Day 10.java
@@ -0,0 +1,26 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        byte a = 2;
+        byte b = 4;
+        byte c = 6;
+        byte d = 8;
+        byte e = 10;
+        byte f = 12;
+        byte g = 14;
+        byte h = 16;
+        byte i = 18;
+        byte j = 20;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+}
