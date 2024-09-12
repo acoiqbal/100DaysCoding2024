d4ff19ea5a45262f87a16264846dfc1daec9a9df
acoiqbal <acoikbalikbal@gmail.com>
2024-09-12 17:34:17 +0800

    Hari 11

---
+++ Day 11.java
@@ -0,0 +1,26 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        short a = 20;
+        short b = 40;
+        short c = 60;
+        short d = 80;
+        short e = 100;
+        short f = 120;
+        short g = 140;
+        short h = 160;
+        short i = 180;
+        short j = 200;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+}
