c6e6087f99a404097326680e8a217f22a5889ea9
acoiqbal <acoikbalikbal@gmail.com>
2024-09-13 17:45:44 +0800

    hari 12

---
+++ Day 12.java
@@ -0,0 +1,33 @@
+package day.pkg12;
+
+public class Day12 {
+
+    public static void main(String[] args) {
+        int kucing, harimau, ayam, tikus, kambing, gajah;
+        
+        kucing = 7;
+        harimau = 5;
+        ayam = 9;
+        tikus = 1;
+        kambing = 3;
+        gajah = 27;
+        
+         System.out.printf("bilangan ganjil : %d,%d, %d, %d, %d, %d ", kucing, harimau, ayam, tikus, kambing, gajah);
+         
+        int macan = 8;
+        int angsa = 10;
+        int bebek = 4;
+        int merak = 100;
+        
+         System.out.println("bilangan genap :"+ macan + "," + angsa + "," + bebek + "," + merak);
+        
+        int sapi = 13;
+        int merpati = 23;
+        int elang = 5;
+        int pipit = 11;
+        
+         System.out.printf("bilangan prima :  %d,%d, %d, %d ", sapi, merpati, elang, pipit);
+        
+    }
+
+}
