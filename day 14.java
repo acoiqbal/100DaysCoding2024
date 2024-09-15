a78cbfb0fdac4b7af60520732feb9a3cdd04cad8
acoiqbal <acoikbalikbal@gmail.com>
2024-09-15 17:47:34 +0800

    hari 14

---
+++ day 14.java
@@ -0,0 +1,30 @@
+package day.pkg4;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+        long a, b, c, d, e, f, g, h, i, j;
+        a = -1000;
+        b = 1000;
+        c = 2000;
+        d = 3000;
+        e = 4000;
+        f = 5000;
+        g = 6000;
+        h = 7000;
+        i = 8000;
+        j = 9000;
+        
+        System.out.printf("Deklarasi Long : %d, %d, %d, %d, %d, ", a, b, c, d, e);
+        System.out.println();
+        System.out.printf("Deklarasi Long : %d, %d, %d, %d, %d, ", f, g, h, i, j);
+        System.out.println();
+        
+        long k = 10000;
+        long l = 20000;
+        long m = 30000;
+        long n = 40000;
+        
+        System.out.println("inialisasi long : "+k+","+l+","+m+","+n);
+    }
+}
