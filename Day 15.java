0f67923dc469483f1551170e67a1a35af6e02d56
acoiqbal <acoikbalikbal@gmail.com>
2024-09-16 17:11:31 +0800

    Hari 15

---
+++ Day 15.java
@@ -0,0 +1,57 @@
+package day.pkg4;
+
+public class Day15 {
+
+    public static void main(String[] args) {
+        double a, b, c, d, e;
+        a = 7.4321898;
+        b = 4.1237659;
+        c = 3.6754986;
+        d = 6.2985421;
+        e = 9.3427654;
+        
+        System.out.printf("double : %2.1f\n",a);
+        System.out.printf("double : %3.2f\n",b);
+        System.out.printf("double : %5.3f\n",c);
+        System.out.printf("double : %5.4f\n",d);
+        System.out.printf("double : %5.5f\n",e);
+        
+        System.out.println("-------------------");
+        
+        double f = 3.87564738234;
+        double g = 8.73622673663;
+        double h = 5.33476284788;
+        double i = 4.74326246834;
+        double j = 2.49782479347;
+        double k = 6.74736348487;
+        double l = 1.46374567583;
+        double m = 9.47756875749;
+        double n = 7.38658739427;
+        
+        System.out.println(String.format ("double : %2.7f\n",f));
+        System.out.println(String.format ("double : %2.7f\n",g));
+        System.out.println(String.format ("double : %2.7f\n",h));
+        System.out.println(String.format ("double : %2.7f\n",i));
+        System.out.println(String.format ("double : %2.7f\n",j));
+        System.out.println(String.format ("double : %2.7f\n",k));
+        System.out.println(String.format ("double : %2.7f\n",l));
+        System.out.println(String.format ("double : %2.7f\n",m));
+        System.out.println(String.format ("double : %2.7f\n",n));
+        
+        System.out.println("----------------------");
+        
+        double pi = 22.0 / 7.0;
+        System.out.println(String.format("Double : %2.2f",pi));
+        System.out.println(String.format("Double : %2.3f",pi));
+        System.out.println(String.format("Double : %2.4f",pi));
+        System.out.println(String.format("Double : %2.5f",pi));
+        System.out.println(String.format("Double : %2.6f",pi));
+        System.out.println(String.format("Double : %2.7f",pi));
+        System.out.println(String.format("Double : %2.8f",pi));
+        System.out.println(String.format("Double : %2.9f",pi));
+        System.out.println(String.format("Double : %2.10f",pi));
+        System.out.println(String.format("Double : %2.11f",pi));
+        
+        
+    }
+}
