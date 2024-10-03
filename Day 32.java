0897b62b921310f31933dd6f424fba39e0dc3306
acoiqbal <acoikbalikbal@gmail.com>
2024-10-03 08:20:05 +0800

    Hari 32

---
+++ Day 32.java
@@ -0,0 +1,41 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 123;
+        byte b = 12;
+        short c = 34;
+        long d = 456;
+        
+        double e = 12.3;
+        float f = 45.6f;
+        
+        char g = 'S';
+        
+        boolean h = true;
+        
+        String i = String.valueOf(a);
+        String j = String.valueOf(b);
+        String k = String.valueOf(c);
+        String l = String.valueOf(d);
+        String m = String.valueOf(e);
+        String n = String.valueOf(f);
+        String o = String.valueOf(g);
+        String p = String.valueOf(h);
+        
+        // Menampilkan hasil konversi
+        System.out.println("A = " + i );
+        System.out.println("B = " + j);
+        System.out.println("C = " + k);
+        System.out.println("D = " + l);
+        
+        System.out.println("E = " + m);
+        System.out.println("F = " + n);
+        
+        System.out.println("G = " + o);
+        
+        System.out.println("H = " + p);
+        
+        System.out.println("Sekian");
+    }
+}
+
