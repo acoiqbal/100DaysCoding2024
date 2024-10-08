56381a6cbb0181f6393c84472b9fc13fe7126b67
acoiqbal <acoikbalikbal@gmail.com>
2024-10-08 20:24:01 +0800

    Hari 37

---
+++ Day 37.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+       int a, b;
+        a = 12;
+        b = 10;
+        int hasil1 = a + b;
+
+        a += 2;
+        b -= 3;
+
+        System.out.println("hasil penjumlahan A dan B = " + a);
+        System.out.println("Hasil pengurangan C dan D = " + b);
+      
+        System.out.println("----------------------");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka = ");
+        int c = sc.nextInt();
+        System.out.print("masukkan angka = ");
+        int d = sc.nextInt();
+        c += d;
+       
+        System.out.println("Hasil penjumlah A dan B = " + c);
+        
+        System.out.println("----------------------");
+        System.out.print("Masukkan angka = ");
+        int e = sc.nextInt();
+
+        System.out.print("masukkan angka = ");
+        int f = sc.nextInt();
+        
+        e -= f;
+       
+        System.out.println("Hasil penjumlah A dan B = " + e);
+        
+        sc.close();
+    }
+}
+
