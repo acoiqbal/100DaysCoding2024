25b0b2127d2e29a6f2ffdaf3548599cace03e7e7
acoiqbal <acoikbalikbal@gmail.com>
2024-10-20 15:08:52 +0800

    Hari 49

---
+++ Day 49.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        int a, b;
+        a = 65;
+        b = 75;
+        
+        if (a >= 70 ) {
+            System.out.println("nilai anda memenuhi");
+        }else if (a == 65){
+            System.out.println("nilai anda error");
+        }
+        
+        if (b <= 70) {
+            System.out.println("nilai anda tidak memenuhi");
+        }else if (b == 75){
+            System.out.println("nilai anda memenuhi");
+        }
+        
+        System.out.println("----------------------");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai : ");
+        int c = sc.nextInt();
+        
+        if (c >= 70){
+            System.out.println("anda lulus");
+        }else if (c <= 70){
+            System.out.println("anda tidak lulus");
+        }
+        
+        System.out.print("Masukkan nilai : ");
+        int d = sc.nextInt();
+        
+        if (d <= 70) {
+            System.out.println("anda tidak lulus");
+        }else if (d >= 70){
+            System.out.println("anda lulus");
+        }
+        
+        sc.close();
+    }
+}
